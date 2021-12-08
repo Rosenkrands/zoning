b4696782b5b919770e8ca3de95d8013ba3718ef3
casper pedersen <cbpe17@student.aau.dk>
2021-12-08 09:06:20 +0100

    regresion update

--- regression.R
+++ regression.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(cowplot)
+library(xtable)
 
 regression_data <- readRDS("./regression-data.rds")
 
@@ -30,10 +31,22 @@ fulrat_gg <- ggplot(regression_data, aes(x = `Solution method`, y = `Fulfillment
     geom_boxplot()
 plot_grid(perresp_gg, fulrat_gg, ncol = 2, axis = 'l')
 
+# Appendix
+solmet_gg <- ggplot(regression_data, aes(x = `Solution method`, y = `Fulfillment ratio`) ) +
+  geom_boxplot() 
+arvar_gg <- ggplot(regression_data, aes(x = `Arrival rate variance`, y = `Fulfillment ratio`) ) +
+  geom_boxplot() 
+nuav_gg <- ggplot(regression_data, aes(x = `Number of UAVs`, y = `Fulfillment ratio`) ) +
+  geom_boxplot() 
+plot_grid(solmet_gg, arvar_gg, nuav_gg, ncol = 3, axis = 'l', rel_widths = c(1.4, 1, 1))
+
 ### Statistical tests
 summary(Mean_response_regression)
+print(xtable(summary(Mean_response_regression)))
 summary(Ninety_response_regression)
+print(xtable(summary(Ninety_response_regression)))
 summary(Fulfillment_ration_regression)
+print(xtable(summary(Fulfillment_ration_regression)))
 
 ### Interactions
 # Compare TOT and Kmeans
@@ -48,13 +61,22 @@ summary(Mean_response_regression_TOTK_ninety)
 Mean_response_regression_TOTK_fulfill <- lm(`Fulfillment ratio` ~ `Solution method` + `Arrival rate variance` + `Number of UAVs` + `Solution method`:`Arrival rate variance`, data = regression_data_TOTK)
 summary(Mean_response_regression_TOTK_fulfill)
 
+#Number of UAVs
+Mean_response_regression_TOTK_UAV <- lm(`Mean response` ~ `Arrival rate variance` + `Number of UAVs`*`Solution method` , data = regression_data_TOTK)
+summary(Mean_response_regression_TOTK_UAV)
+print(xtable(summary(Mean_response_regression_TOTK_UAV)))
+
+Mean_response_regression_TOTK_UAV <- lm(`90th percentile response` ~ `Arrival rate variance` + `Number of UAVs`*`Solution method` , data = regression_data_TOTK)
+summary(Mean_response_regression_TOTK_UAV)
+print(xtable(summary(Mean_response_regression_TOTK_UAV)))
+
 
 #Compare ARV and TOT
 regression_data_TOTARV <-  regression_data %>% filter(`Solution method` %in% c('GA:ARV', 'GA:TOT'))
 Mean_response_regression_TOTARV <- lm(`Mean response` ~ `Solution method` + `Arrival rate variance` + `Number of UAVs` + `Solution method`:`Arrival rate variance`, data = regression_data_TOTARV)
 summary(Mean_response_regression_TOTARV)
 
-k
+
 
 
 
